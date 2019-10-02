b6a6a48204dee520da0d0cdf1550ceb59d09ff13
Jose Manuel <you@example.com>
2019-10-02 12:23:23 -0500

    Conexion a la bd lista

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://santi_quintero:pagetour2019@pagetour-ucdqg.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const db = client.db("Pagetour");
-  // perform actions on the collection object
-  console.log("DB is connected");
-  client.close();
-});
 
+const mongoose = require('mongoose');
+const URI = "mongodb+srv://santi_quintero:pagetour2019@pagetour-ucdqg.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(URI)
+  .then(db => {
+    console.log('Db is connected')
+    mongoose.connection.close()
+  })
+  .catch(error => console.error(error));
+
+module.exports = mongoose;
